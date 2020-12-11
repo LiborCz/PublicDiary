da8eac8813e5aefdfe4f680d6c480e4038d48638
Libor Pilný <libor.pilny@gmail.com>
2020-12-11 22:53:27 +0100

    Cleaning the app

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 // ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:3000/', 'http://localhost:8080/', 'https://liborcz-lostandfound.herokuapp.com']
+const whitelist = ['http://localhost:3000/', 'http://localhost:8080/', 'https://public-diary.herokuapp.com']
 const corsOptions = {
   origin: function (origin, callback) {
     console.log("** Origin of request " + origin)
