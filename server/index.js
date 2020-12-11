c0558ad2bfdd6212a284cd9072a685c6eb933cd9
Libor Pilný <libor.pilny@gmail.com>
2020-12-11 22:32:40 +0100

    Moving server to Directory

--- server/index.js
+++ server/index.js
@@ -38,6 +38,8 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, 'client/build')));
 
+  console.log("dirname: " + __dirname);
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
